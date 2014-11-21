f6407f5f5f11ca9915bd2cb486770709024eb5bb
Qpz <convert.pierre@gmail.com>
2014-11-21 10:52:06 +0100

    color2 added

---
+++ sources/com/mint/web/apps/webgl/programs/color2/setup.js
@@ -0,0 +1,41 @@
+
+
+program.initialize = function(){
+
+	
+	// matrices
+	this.loc_Uniform_matrix_MVP = gl.getUniformLocation(this.handle, "ModelViewProjection_Matrix");
+	
+	//attributes locations
+	this.loc_Attribute_vertex = gl.getAttribLocation(this.handle, "vertex");
+	
+	// material
+	this.loc_Uniform_color = gl.getUniformLocation(this.handle, "color");	
+	
+};
+
+
+
+program.loadView = function(view){
+}
+
+program.loadEnvironment = function(environment){
+};
+
+
+program.loadStyle = function(style){
+
+	// material
+	gl.uniform4fv(this.loc_Uniform_color, style.color);
+};
+
+
+program.loadShape = function(shape){
+	
+	// matrices
+	gl.uniformMatrix4fv(this.loc_Uniform_matrix_MVP, false, shape.matrix_ProjectionViewModel.c);
+	
+	// attributes
+	shape.vertex.bind(this.loc_Attribute_vertex);
+};
+
