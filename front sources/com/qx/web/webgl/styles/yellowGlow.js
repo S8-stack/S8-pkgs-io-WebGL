2fae09967299454c181ffd566008242a2db480c6
pc <pc@192.168.12.244>
2018-11-19 20:40:26 +0100

    done

--- front sources/com/qx/web/webgl/styles/yellowGlow.js
+++ front sources/com/qx/web/webgl/styles/yellowGlow.js
@@ -3,8 +3,8 @@
 
 style.initialize = function(){
 	this.programId = "glow";
-	this.glowColor = [1.0, 1.0, 0.8, 0.0];
-	this.outlineColor = [1.0, 1.0, 0.1, 0.0];
+	this.glowColor = [1.0, 1.0, 0.0, 0.0];
+	this.outlineColor = [0.5, 0.5, 0.1, 0.0];
 };
 
 
