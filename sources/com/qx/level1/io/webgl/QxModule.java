6ffbefc95c520cf50725722f3380bf3bb640700e
pc <pc@pierres-mbp>
2019-11-14 19:48:41 +0100

    reflecting updates

--- sources/com/qx/level1/io/webgl/QxModule.java
+++ sources/com/qx/level1/io/webgl/QxModule.java
@@ -2,6 +2,8 @@ package com.qx.level1.io.webgl;
 
 import java.io.InputStream;
 
+import com.qx.level0.web.sources.ModuleWebSources;
+
 
 /**
  * Root point for computing path to further load client-side resources.
@@ -16,7 +18,8 @@ public class QxModule extends com.qx.level0.utilities.loader.QxModule {
 		@Override
 		public InputStream getResource(String pathname) {
 			return QxModule.class.getResourceAsStream(pathname);
-		}
-		
+		}	
 	};
+	
+	public final static ModuleWebSources WEB_SOURCES = new ModuleWebSources("webgl", LOADER);
 }
--- tests/com/qx/io/webgl/tests/WebGL_UnitTest03.java
+++ tests/com/qx/io/webgl/tests/WebGL_UnitTest03.java
@@ -1,12 +1,12 @@
 package com.qx.io.webgl.tests;
 
-import com.qx.level0.web.sources.WebSources;
+import com.qx.level0.web.sources.ModuleWebSources;
 
 public class WebGL_UnitTest03 {
 
 	public static void main(String[] args) throws Exception {
 		
-		WebSources sources = new WebSources("webgl", com.qx.level1.io.webgl.QxModule.LOADER);
+		ModuleWebSources sources = new ModuleWebSources("webgl", com.qx.level1.io.webgl.QxModule.LOADER);
 		System.out.println(sources.JS_getCatalog());
 		System.out.println(sources);
 	}
