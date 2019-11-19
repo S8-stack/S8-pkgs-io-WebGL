75c334ec9ac43f5c0119afb72757c0bb106cd234
pc <pc@macbook-pro-1.home>
2019-11-19 20:47:49 +0100

    update

--- sources/module-info.java
+++ sources/module-info.java
@@ -13,11 +13,11 @@ module com.qx.io.webgl {
 	exports com.qx.level1.io.webgl.appearances;
 	exports com.qx.io.webgl.tests;
 
-	requires transitive com.qx.blocks;
 	requires transitive com.qx.level0.utilities;
 	requires transitive com.qx.level0.lang.xml;
 	requires transitive com.qx.level0.maths;
 	requires transitive com.qx.level1.maths;
 	requires transitive com.qx.level1.io.bohr;
-	requires com.qx.level0.web;
+	requires transitive com.qx.level0.web;
+	requires transitive com.qx.level0.blocks;
 }
