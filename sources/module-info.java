85c4ce93c2a160bf6685ec2efbb47ce1995d83b0
pierreconvert <pierre.convert@qxloop.com>
2021-12-15 09:40:14 +0100

    update

--- sources/module-info.java
+++ sources/module-info.java
@@ -1,25 +1,12 @@
-/**
- * 
- */
-/**
- * @author pc
- *
- */
 module com.qx.level1.io.webgl {
-	
-	exports com.qx.level1.io.webgl._meta;
-	exports com.qx.level1.io.webgl;
-	exports com.qx.level1.io.webgl.appearances;
-	exports com.qx.level1.io.webgl.shapes;
+	exports com.qx.level1.io.webgl.tests.loading;
 	exports com.qx.level1.io.webgl.programs;
+	exports com.qx.level1.io.webgl.tests;
+	exports com.qx.level1.io.webgl._meta;
+	exports com.qx.level1.io.webgl.util;
+	exports com.qx.level1.io.webgl.shapes;
+	exports com.qx.level1.io.webgl.appearances;
+	exports com.qx.level1.io.webgl;
 
-	requires transitive com.qx.level1.maths;
-	
-	requires transitive com.qx.level0.utilities;
-	requires transitive com.qx.level0.lang.xml;
-	requires transitive com.qx.level0.maths;
-	requires transitive com.qx.level0.web;
-	requires transitive com.qx.level0.db.blocks;
-	requires transitive com.qx.level0.io.bohr;
-	
+	requires com.qx.level1.io.webgl;
 }
