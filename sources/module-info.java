e815cbaa13a483f7d145bb51cc36f96444c56716
Pierre Convert <pierreconvert@macbook-pro-2.home>
2022-12-03 09:08:36 +0100

    MASSIVE REBASE

--- sources/module-info.java
+++ sources/module-info.java
@@ -1,12 +1,10 @@
-module com.S8.sets.poly3d {
-	exports com.qx.level1.io.webgl.tests.loading;
-	exports com.s8.sets.poly3d.programs;
-	exports com.qx.level1.io.webgl.tests;
-	exports com.s8.sets.poly3d._meta;
-	exports com.s8.sets.poly3d.util;
+module com.s8.io.swgl {
 	exports com.s8.sets.poly3d.shapes;
-	exports com.s8.sets.poly3d.appearances;
+	exports com.qx.level1.io.webgl.tests.loading;
 	exports com.s8.sets.poly3d;
-
-	requires com.qx.level1.io.webgl;
+	exports com.qx.level1.io.webgl.tests;
+	exports com.s8.sets.poly3d.util;
+	exports com.s8.sets.poly3d.programs;
+	exports com.s8.sets.poly3d._meta;
+	exports com.s8.sets.poly3d.appearances;
 }
