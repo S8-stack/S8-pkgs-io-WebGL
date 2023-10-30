d0a9a708c75ad64c7739263c841804104f6f2c77
Pierre Convert <pierreconvert@localhost>
2023-10-30 22:14:49 +0100

    S8-core related renamings

--- sources/com/s8/io/swgl/SWGL_Root.java
+++
@@ -1,6 +0,0 @@
-package com.s8.io.swgl;
-
-public class SWGL_Root {
-
-	public final static String WEB = "/s8-io-swgl/";
-}
--- sources/module-info.java
+++ sources/module-info.java
@@ -1,33 +1,33 @@
 
-module com.s8.io.swgl {
+module com.s8.pkgs.io.swgl {
 	
 	
 	
 
-	exports com.s8.io.swgl.material;
-	exports com.s8.io.swgl.maths;
+	exports com.s8.pkgs.io.swgl.material;
+	exports com.s8.pkgs.io.swgl.maths;
 	
-	exports com.s8.io.swgl.render;
+	exports com.s8.pkgs.io.swgl.render;
 
 
-	exports com.s8.io.swgl.scene;
+	exports com.s8.pkgs.io.swgl.scene;
 	
-	exports com.s8.io.swgl.scene.pipes;
-	exports com.s8.io.swgl.scene.pipes.color2;
-	exports com.s8.io.swgl.scene.pipes.mat01;
-	exports com.s8.io.swgl.scene.pipes.phys2;
-	exports com.s8.io.swgl.scene.pipes.picking;
-	exports com.s8.io.swgl.scene.pipes.standard;
+	exports com.s8.pkgs.io.swgl.scene.pipes;
+	exports com.s8.pkgs.io.swgl.scene.pipes.color2;
+	exports com.s8.pkgs.io.swgl.scene.pipes.mat01;
+	exports com.s8.pkgs.io.swgl.scene.pipes.phys2;
+	exports com.s8.pkgs.io.swgl.scene.pipes.picking;
+	exports com.s8.pkgs.io.swgl.scene.pipes.standard;
 	
-	exports com.s8.io.swgl.scene.environment;
-	exports com.s8.io.swgl.scene.environment.lights;
+	exports com.s8.pkgs.io.swgl.scene.environment;
+	exports com.s8.pkgs.io.swgl.scene.environment.lights;
 	
-	exports com.s8.io.swgl.scene.models;
+	exports com.s8.pkgs.io.swgl.scene.models;
 	
-	exports com.s8.io.swgl.scene.view;
+	exports com.s8.pkgs.io.swgl.scene.view;
 	
 
-	exports com.s8.io.swgl.utilities;
+	exports com.s8.pkgs.io.swgl.utilities;
 	
 	
 	
